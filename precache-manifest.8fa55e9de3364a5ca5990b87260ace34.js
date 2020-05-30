d21aeb891c93f425914df355c1720d5a6751ee0a
Backend Server <backendserver@gmail.com>
2020-05-30 21:12:09 +0700

    dfs

---
+++ precache-manifest.8fa55e9de3364a5ca5990b87260ace34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd2953339b8ea3221100",
+    "url": "/static/css/main.aa2a4b4a.chunk.css"
+  },
+  {
+    "revision": "bd2953339b8ea3221100",
+    "url": "/static/js/main.bd295333.chunk.js"
+  },
+  {
+    "revision": "02fb5f3a79d449eccc64",
+    "url": "/static/js/1.02fb5f3a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dc087d81e9f3fa9f740302a2db8a7d62",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fd9d8da614dbcee83e8ed099ea827c02.js"
+  "/precache-manifest.8fa55e9de3364a5ca5990b87260ace34.js"
 );
 
 workbox.clientsClaim();
