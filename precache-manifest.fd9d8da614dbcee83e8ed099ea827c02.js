6d724e9c1c5bab36ee0c97abdf7671bf9d6ee34e
Backend Server <backendserver@gmail.com>
2020-05-30 18:42:06 +0700

    Deploy Hosting

---
+++ precache-manifest.fd9d8da614dbcee83e8ed099ea827c02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab381d509e829b27d3bd",
+    "url": "/static/css/main.aa2a4b4a.chunk.css"
+  },
+  {
+    "revision": "ab381d509e829b27d3bd",
+    "url": "/static/js/main.ab381d50.chunk.js"
+  },
+  {
+    "revision": "02fb5f3a79d449eccc64",
+    "url": "/static/js/1.02fb5f3a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "52e6b9ce700f05783c2e7adf226fa9e3",
+    "url": "/index.html"
+  }
+];
